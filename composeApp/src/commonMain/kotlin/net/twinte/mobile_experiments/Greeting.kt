d31b91e789bdddddff167e5f03b777500b12235d
Ryoga-exe <git@ryoga.dev>
2026-05-09 02:49:46 +0900

    reset

--- composeApp/src/commonMain/kotlin/net/twinte/mobile_experiments/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package net.twinte.mobile_experiments
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/net/twinte/mobile_experiments/core/domain/AuthProvider.kt
+++
@@ -1,7 +0,0 @@
-package net.twinte.mobile_experiments.core.domain
-
-enum class AuthProvider {
-    Google,
-    Apple,
-    Twitter,
-}
